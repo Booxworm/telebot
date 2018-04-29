110921e03b132f877a1f277e3e6da195e2904fa5
Booxworm <lee.wonnjen@gmail.com>
2018-04-29 19:57:05 +0800

    Added .env file

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const Telebot = require("node-telegram-bot-api");
-const config = require("./config");
 
-const api_key = config.API_KEY || process.env.API_KEY;
+const api_key = process.env.API_KEY;
 
 const bot = new Telebot(api_key, { polling: true });
 
