533c5235ec483bae256d479d309b3e86d645c500
Booxworm <lee.wonnjen@gmail.com>
2018-04-29 23:45:57 +0800

    Blocked remind for now

--- index.js
+++ index.js
@@ -2,18 +2,19 @@ const Telebot = require("node-telegram-bot-api");
 const db = require("./db");
 const api_key = process.env.API_KEY;
 const bot = new Telebot(api_key, { polling: true });
+const table = "mytable";
 
 bot.onText(/\/start/, (msg) => {
   bot.sendMessage(msg.chat.id, "Welcome");
 });
 
 bot.onText(/\/list/, (msg) => {
-  db.query("SELECT * FROM mytable", (err, res) => {
+  db.query(`SELECT * FROM ${table}`, (err, res) => {
     if(err) console.log(err.stack);
     else {
       var msgList = [];
       for (let row of res.rows) {
-        var message = row.date + " - " + row.message;
+        var message = `${row.date} - ${row.message}`;
         msgList.push(message);
       }
       db.end();
@@ -22,15 +23,17 @@ bot.onText(/\/list/, (msg) => {
   });
 });
 
+/*
 bot.onText(/\/remind/, (msg) => {
   var text = msg.text.split(" ");
   text.shift();
   var name = msg.from.id;
   var date = text.shift();
   var reminder = text.join(" ");
-  db.query("INSERT INTO mytable (name, date, message) VALUES (${name}, ${date}, ${reminder})", (err, res) => {
+  db.query(`INSERT INTO ${table} (name, date, message) VALUES (${name}, ${date}, ${reminder})`, (err, res) => {
     if (err) console.log(err.stack);
   });
   db.end();
   bot.sendMessage(msg.chat.id, "Reminder added!");
 });
+*/
