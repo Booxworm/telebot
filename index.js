6bf5abf6051e923a3f536974a050b6575624bf18
Booxworm <lee.wonnjen@gmail.com>
2018-04-29 20:50:54 +0800

    Adding pg

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const Telebot = require("node-telegram-bot-api");
-
 const api_key = process.env.API_KEY;
-
 const bot = new Telebot(api_key, { polling: true });
+
+const { Client } = require("pg");
+const client = new Client();
+client.connect();
 
 bot.onText(/\/start/, (msg) => {
   bot.sendMessage(msg.chat.id, "Welcome");
+});
+
+bot.onText(/\/query/, (msg) => {
+  client.query("SELECT # FROM mytable", (err, res) => {
+    if(err) throw err;
+    bot.sendMessage(msg.chat.id, res.rows[0]);
+    client.end();
+  });
 });
