ab79a01c3bc451d50b759023a4239a06d1c87c18
Booxworm <lee.wonnjen@gmail.com>
2018-04-29 19:51:37 +0800

    Added api key to heroku env

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const Telebot = require("node-telegram-bot-api");
 const config = require("./config");
 
-const bot = new Telebot(config.API_KEY, { polling: true });
+const api_key = config.API_KEY || process.env.API_KEY;
+
+const bot = new Telebot(api_key, { polling: true });
 
 bot.onText(/\/start/, (msg) => {
   bot.sendMessage(msg.chat.id, "Welcome");
