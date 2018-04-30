9617b27599711b82e60c093c4011e69152ac747b
Booxworm <lee.wonnjen@gmail.com>
2018-04-30 13:09:59 +0800

    Shifted queries into db.js

--- db.js
+++ db.js
@@ -8,12 +8,26 @@ const client = new Client({
   //connectionString: process.env.DATABASE_URL,
   ssl: true
 });
+const table = "mytable";
 
 client.connect();
 
 module.exports = {
-  query: function(query, values, callback){
-    return client.query(query, values, callback);
+  list: function(name, callback){
+    client.query(`SELECT * FROM ${table} WHERE name='${name}';`, (err, res) => {
+      if(err) console.log(err.stack);
+      else callback(res);
+    });
+  },
+  insert: function(name, date, message){
+    client.query(`INSERT INTO ${table} (name, date, message) VALUES ('${name}', '${date}', '${message}');`, (err, res) => {
+      if(err) console.log(err.stack);
+    });
+  },
+  delete: function(name){
+    client.query(`DELETE FROM ${table} WHERE name='${name}';`, (err, res) => {
+      if(err) console.log(err.stack);
+    });
   },
   end: function(){
     client.end();
--- index.js
+++ index.js
@@ -2,25 +2,20 @@ const Telebot = require("node-telegram-bot-api");
 const db = require("./db");
 const api_key = process.env.API_KEY;
 const bot = new Telebot(api_key, { polling: true });
-const table = "mytable";
 
 bot.onText(/\/start/, (msg) => {
   bot.sendMessage(msg.chat.id, "Welcome");
 });
 
 bot.onText(/\/list/, (msg) => {
-  db.query(`SELECT * FROM ${table};`, (err, res) => {
-    if(err) {
-      console.log(err.stack);
-    } else if (!res.rows.length) {
+  db.list(msg.from.id, (res) => {
+    if (!res.rows.length) {
       bot.sendMessage(msg.chat.id, "You have no saved reminders!");
-    }else {
+    } else {
       var msgList = [];
       for (let row of res.rows) {
-        if (row.name.trim() == msg.from.id){
-          var message = `${row.date.trim()} - ${row.message.trim()}`;
-          msgList.push(message);
-        }
+        var message = `${row.date.trim()} - ${row.message.trim()}`;
+        msgList.push(message);
       }
       bot.sendMessage(msg.chat.id, msgList.join("\n"));
     }
@@ -30,14 +25,23 @@ bot.onText(/\/list/, (msg) => {
 bot.onText(/\/remind/, (msg) => {
   var text = msg.text.split(" ");
   text.shift();
-  var name = msg.from.id;
   var date = text.shift();
   var message = text.join(" ");
-  db.query(`INSERT INTO ${table} (name, date, message) VALUES ('${name}', '${date}', '${message}');`);
-  bot.sendMessage(msg.chat.id, "Reminder added!");
+  if(!message) {
+    bot.sendMessage(msg.chat.id, "Please type in a reminder!");
+  } else if (!date) {
+    bot.sendMessage(msg.chat.id, "Please type in a date!");
+  } else {
+    db.insert(msg.from.id, date, message);
+    bot.sendMessage(msg.chat.id, "Reminder added!");
+  }
 });
 
 bot.onText(/\/remove/, (msg) => {
-  db.query(`DELETE FROM ${table} WHERE name='${msg.from.id}';`);
+  db.delete(msg.from.id);
   bot.sendMessage(msg.chat.id, "Reminders removed!");
+});
+
+bot.onText(/\/test/, (msg) => {
+
 });
